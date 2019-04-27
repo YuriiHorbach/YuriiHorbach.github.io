b8a37a07bdb14b303f27ca1f2d9b61398600856d
southl <gyrii84@gmail.com>
2019-04-27 10:13:57 +0300

    add tinyone landing project to portfolio

---
+++ projects/tinyone/js/main.js
@@ -0,0 +1,17 @@
+$(function(){
+    
+$('.slick').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 1,
+  prevArrow: false,
+  nextArrow: false
+  
+  
+});
+});
+
+
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJtYWluLmpzIl0sInNvdXJjZXNDb250ZW50IjpbIiQoZnVuY3Rpb24oKXtcclxuICAgIFxyXG4kKCcuc2xpY2snKS5zbGljayh7XHJcbiAgZG90czogdHJ1ZSxcclxuICBpbmZpbml0ZTogZmFsc2UsXHJcbiAgc3BlZWQ6IDMwMCxcclxuICBzbGlkZXNUb1Nob3c6IDEsXHJcbiAgcHJldkFycm93OiBmYWxzZSxcclxuICBuZXh0QXJyb3c6IGZhbHNlXHJcbiAgXHJcbiAgXHJcbn0pO1xyXG59KTtcclxuXHJcblxyXG4iXSwiZmlsZSI6Im1haW4uanMifQ==
